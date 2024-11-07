2a8dd9ae1d689aecea97d616c770ab266f4345ec
Kimathi isaac <Isaackimathi@gmail.com>
2024-11-07 16:46:27 +0300

    obfuscated

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Script for handling personal data
+"""
+
+from typing import List
+import re
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+        separator: str) -> str:
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
+    return message
+
+
