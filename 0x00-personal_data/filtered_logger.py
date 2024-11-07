472c0b5250ee5c83210820d2443b3be5f05ae886
Kimathi isaac <Isaackimathi@gmail.com>
2024-11-07 16:57:46 +0300

    obfuscated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,14 +2,31 @@
 """
 Script for handling personal data
 """
-
 from typing import List
 import re
+import logging
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-        separator: str) -> str:
+
+# # PII fields to be redacted
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                         message: str, separator: str) -> str:
+    """
+    Replaces sensitive information in a message with a redacted value
+    based on the list of fields to obfuscate
+
+    Args:
+        fields: list of fields to redact
+        redaction: the value to use for redaction
+        message: the string message to filter
+        separator: the separator to use between fields
+
+    Returns:
+        The filtered string message with redacted values
+    """
     for f in fields:
-        message = re.sub(f'{f}=.*?{separator}', f'{f}={redaction}{separator}', message)
+        message = re.sub(f'{f}=.*?{separator}',
+                f'{f}={redaction}{separator}', message)
     return message
-
-
