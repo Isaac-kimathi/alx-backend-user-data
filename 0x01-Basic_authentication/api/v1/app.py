18c3a511a9de40838596cdf97b10b88c90a3323e
Kimathi isaac <Isaackimathi@gmail.com>
2024-11-12 17:14:13 +0300

    authorized error handling

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-
+from typing import Literal
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -20,6 +20,11 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def unauthorized(error) -> tuple[str, Literal[401]]:
+    """ Unauthorized handler
+    """
+    return jsonify({"error": "unauthorized"}), 401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
