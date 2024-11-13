6fa98dda6862c3c3ea929f13c5f6f29624a7c40f
Kimathi isaac <Isaackimathi@gmail.com>
2024-11-13 13:51:29 +0300

    path_authenticate

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,23 @@ class Auth:
         """
         public methd to authenticate path
         """
-        return false
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
@@ -25,3 +41,5 @@ class Auth:
         method of retrieve the current user based on the request
         """
         return None
+
+
